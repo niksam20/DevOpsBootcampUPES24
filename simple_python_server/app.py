1b5e60ab0513bdf9fe9ccfe063b9cd4c04dcb323
NIKHIL SINGH <nikhilbisht0026@gmail.com>
2024-09-20 10:48:13 +0530

    dockerize the application

--- simple_python_server/app.py
+++ simple_python_server/app.py
@@ -45,4 +45,4 @@ if __name__ == "__main__":
     signal.signal(signal.SIGTERM, handle_sigterm)
     print(f'Server PID={os.getpid()}')
 
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=3500)
